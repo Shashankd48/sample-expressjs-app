dd0f6da000e0a190477dd22f2053dcdb0a4e8f30
Shashank Dubey <jfriday464@gmail.com>
2025-01-06 11:57:30 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+   res.send("Hello World!");
+});
+
+app.get("/status", (req, res) => {
+   res.status(200).json({ name: "Sanjay Singh", age: 24 });
+});
+
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`);
+   console.log(`Open in your browser http://localhost:${port}`);
+});
