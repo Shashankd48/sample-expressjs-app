bf88446370b3b175cee8a6e8935ea4cc3c6d15d5
Shashank Dubey <jfriday464@gmail.com>
2026-06-15 16:31:09 +0530

    Test

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get("/new", (req, res) => {
    res.status(200).json({ version: "2.0" })
 })
 
+app.get("/api", (req, res) => {
+   res.status(200).json({ version: "2.0" })
+})
+
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`);
    console.log(`Open in your browser http://localhost:${port}`);
